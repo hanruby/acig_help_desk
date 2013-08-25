372a546bfb1174005dc9c7c2b70975c1fd98abc1
Ubaid Khan <ubaidkhan88@gmail.com>
2013-08-25 09:50:59 +0300

    removed extra pages and integrated into single page

--- Tickets/assigned.aspx
+++ Tickets/assigned.aspx
@@ -3,12 +3,29 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
-  <asp:HiddenField ID="hdnFldScope" runat="server" />
     <h3>
-        <asp:Label ID="lblHeading" runat="server" Text="Label"></asp:Label>
+        <asp:Label ID="lblOpen" runat="server" Text=""></asp:Label>
     </h3>
-    <asp:GridView ID="gvTickets" runat="server" Width="100%" CssClass="table table-bordered"
-        EmptyDataText="No Records" OnRowDataBound="gvTickets_RowDataBound">
+    <asp:GridView ID="gvTicketsOpen" runat="server" Width="100%" CssClass="table table-bordered"
+        EmptyDataText="No Records" OnRowDataBound="gvTicketsOpen_RowDataBound"
+        AllowPaging="true" OnPageIndexChanging="gvTicketsOpen_PageIndexChanging" PageSize="10">
     </asp:GridView>
+    <hr />
+    <h3>
+        <asp:Label ID="lblResolved" runat="server" Text=""></asp:Label>
+    </h3>
+    <asp:GridView ID="gvTicketsResolved" runat="server" Width="100%" CssClass="table table-bordered"
+        EmptyDataText="No Records" OnRowDataBound="gvTicketsResolved_RowDataBound"
+        AllowPaging="true" OnPageIndexChanging="gvTicketsResolved_PageIndexChanging" PageSize="10">
+    </asp:GridView>
+    <hr />
+    <h3>
+        <asp:Label ID="lblClosed" runat="server" Text="Label"></asp:Label>
+    </h3>
+    <asp:GridView ID="gvTicketsClosed" runat="server" Width="100%" CssClass="table table-bordered"
+        EmptyDataText="No Records" OnRowDataBound="gvTicketsClosed_RowDataBound" 
+        AllowPaging="true" OnPageIndexChanging="gvTicketsClosed_PageIndexChanging" PageSize="10">
+    </asp:GridView>
+    <hr />
 </asp:Content>
 
--- Tickets/index.aspx.cs
+++ Tickets/index.aspx.cs
@@ -9,38 +9,111 @@ using System.Data;
 
 public partial class Tickets_index : MasterAppPage
 {
-    string _scope, _customScope;
     DataTable dt;
     DataRow dr;
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            BindGvOpenTickets();
+            BindGvResolvedTickets();
+            BindGvClosedTickets();
+            lblOpen.Text = "Pending Tickets!";
+            lblResolved.Text = "Resolved Tickets!";
+            lblClosed.Text = "Closed Tickets!";
+        }
+    }
+
+    protected void gvTicketsOpen_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvTicketsOpen.PageIndex = e.NewPageIndex;
+        BindGvOpenTickets();
+    }
+
+    protected void gvTicketsResolved_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvTicketsResolved.PageIndex = e.NewPageIndex;
+        BindGvResolvedTickets();
+    }
+
+    protected void gvTicketsClosed_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvTicketsClosed.PageIndex = e.NewPageIndex;
+        BindGvClosedTickets();
+    }
+
+    protected void gvTicketsOpen_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType != DataControlRowType.DataRow) return;
+        var text = e.Row.Cells[3].Text;
+        LinkButton lb;
+        lb = new LinkButton();
+        lb.CommandArgument = text;
+        lb.CommandName = "NumClick";
+        lb.Text = "Details";
+        lb.PostBackUrl = "show.aspx?id=" + text;
+        lb.CssClass = "blue-link";
+        e.Row.Cells[3].Controls.Add((Control)lb);
+    }
+
+    protected void gvTicketsResolved_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType != DataControlRowType.DataRow) return;
+        var text = e.Row.Cells[4].Text;
+        LinkButton lb;
+        lb = new LinkButton();
+        lb.CommandArgument = text;
+        lb.CommandName = "NumClick";
+        lb.Text = "Details";
+        lb.PostBackUrl = "show.aspx?id=" + text;
+        lb.CssClass = "blue-link";
+        e.Row.Cells[4].Controls.Add((Control)lb);
+    }
+
+    protected void gvTicketsClosed_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType != DataControlRowType.DataRow) return;
+        var text = e.Row.Cells[5].Text;
+        LinkButton lb;
+        lb = new LinkButton();
+        lb.CommandArgument = text;
+        lb.CommandName = "NumClick";
+        lb.Text = "Details";
+        lb.PostBackUrl = "show.aspx?id=" + text;
+        lb.CssClass = "blue-link";
+        e.Row.Cells[5].Controls.Add((Control)lb);
+    }
+
+    protected void GetHeader(string scope)
+    {
+        dt = new DataTable();
+        dt.Columns.Add(new DataColumn("Open At", typeof(string)));
+        if (scope == "resolved")
+        {
+            dt.Columns.Add(new DataColumn("Resolved At", typeof(string)));
+        }
+        else if (scope == "closed")
+        {
+            dt.Columns.Add(new DataColumn("Resolved At", typeof(string)));
+            dt.Columns.Add(new DataColumn("Closed At", typeof(string)));
+        }
+        dt.Columns.Add(new DataColumn("Category", typeof(string)));
+        dt.Columns.Add(new DataColumn("Assigned To", typeof(string)));
+        dt.Columns.Add(new DataColumn("Details", typeof(string)));
+    }
+
+    protected void BindGvOpenTickets()
+    {
         dr = null;
-        _scope = Request.QueryString["scope"];
-        if (_scope == "closed")
-        {
-            _customScope = "Closed";
-            hdnFldScope.Value = _scope;
-        }
-        else if (_scope == "resolved")
-        {
-            hdnFldScope.Value = _scope;
-            _customScope = "Resolved";
-        }
-        else
-        {
-            _scope = "pending";
-            hdnFldScope.Value = _scope;
-            _customScope = "Open";
-        }
         currentUserId = CurrentUser.Id();
-        GetHeader();
+        GetHeader("open");
         _entity = GetEntity();
         var data = from t in _entity.Tickets
                    join sc in _entity.Sub_Categories
                    on t.Sub_Category_Id equals sc.Id
                    join c in _entity.Categories
                    on sc.Category_Id equals c.Id
-                   where (t.Created_By == currentUserId) && t.State == _customScope
+                   where (t.Created_By == currentUserId) && t.State == "Open"
                    orderby t.Created_By
                    select new
                    {
@@ -55,66 +128,82 @@ public partial class Tickets_index : MasterAppPage
         {
             dr = dt.NewRow();
             dr["Open At"] = x.OpenAt;
-            dr["Category"] = x.CategoryName;
-            dr["Sub Category"] = x.SubCategoryName;
-            if (_scope == "resolved" || _scope == "closed")
-            {
-                dr["Resolved At"] = x.ResolvedAt;
-            }
-            if (_scope == "closed")
-            {
-                dr["Closed At"] = x.ClosedAt;
-            }
+            dr["Category"] = x.CategoryName + " >> " + x.SubCategoryName;
             dr["Details"] = x.Id;
             dt.Rows.Add(dr);
         }
-        gvTickets.DataSource = dt;
-        gvTickets.DataBind();
-        if (_customScope == "Open")
-        {
-            _customScope = "Pending";
-        }
-        lblHeading.Text = _customScope + " Tickets";
+        gvTicketsOpen.DataSource = dt;
+        gvTicketsOpen.DataBind();
     }
 
-    protected void gvTickets_RowDataBound(object sender, GridViewRowEventArgs e)
+    protected void BindGvResolvedTickets()
     {
-        if (e.Row.RowType != DataControlRowType.DataRow) return;
-        int index = 3;
-        if (hdnFldScope.Value == "resolved")
+        dr = null;
+        currentUserId = CurrentUser.Id();
+        GetHeader("resolved");
+        _entity = GetEntity();
+        var data = from t in _entity.Tickets
+                   join sc in _entity.Sub_Categories
+                   on t.Sub_Category_Id equals sc.Id
+                   join c in _entity.Categories
+                   on sc.Category_Id equals c.Id
+                   where (t.Created_By == currentUserId) && t.State == "Resolved"
+                   orderby t.Created_By
+                   select new
+                   {
+                       OpenAt = t.Created_At,
+                       ResolvedAt = t.Resolved_Date,
+                       ClosedAt = t.Closed_Date,
+                       CategoryName = c.Name,
+                       SubCategoryName = sc.Name,
+                       Id = t.Id
+                   };
+        foreach (var x in data)
         {
-            index = 4;
+            dr = dt.NewRow();
+            dr["Open At"] = x.OpenAt;
+            dr["Category"] = x.CategoryName + " >> " + x.SubCategoryName;
+            dr["Resolved At"] = x.ResolvedAt;
+            dr["Details"] = x.Id;
+            dt.Rows.Add(dr);
         }
-        else if (hdnFldScope.Value == "closed")
-        {
-            index = 5;
-        }
-        var text = e.Row.Cells[index].Text;
-        LinkButton lb;
-        lb = new LinkButton();
-        lb.CommandArgument = text;
-        lb.CommandName = "NumClick";
-        lb.Text = "Details";
-        lb.PostBackUrl = "show.aspx?id=" + text;
-        lb.CssClass = "blue-link";
-        e.Row.Cells[index].Controls.Add((Control)lb);
+        gvTicketsResolved.DataSource = dt;
+        gvTicketsResolved.DataBind();
     }
 
-    protected void GetHeader()
+    protected void BindGvClosedTickets()
     {
-        dt = new DataTable();
-        dt.Columns.Add(new DataColumn("Open At", typeof(string)));
-        dt.Columns.Add(new DataColumn("Category", typeof(string)));
-        dt.Columns.Add(new DataColumn("Sub Category", typeof(string)));
-        if (_scope == "resolved")
+        dr = null;
+        currentUserId = CurrentUser.Id();
+        GetHeader("closed");
+        _entity = GetEntity();
+        var data = from t in _entity.Tickets
+                   join sc in _entity.Sub_Categories
+                   on t.Sub_Category_Id equals sc.Id
+                   join c in _entity.Categories
+                   on sc.Category_Id equals c.Id
+                   where (t.Created_By == currentUserId) && t.State == "Closed"
+                   orderby t.Created_By
+                   select new
+                   {
+                       OpenAt = t.Created_At,
+                       ResolvedAt = t.Resolved_Date,
+                       ClosedAt = t.Closed_Date,
+                       CategoryName = c.Name,
+                       SubCategoryName = sc.Name,
+                       Id = t.Id
+                   };
+        foreach (var x in data)
         {
-            dt.Columns.Add(new DataColumn("Resolved At", typeof(string)));
+            dr = dt.NewRow();
+            dr["Open At"] = x.OpenAt;
+            dr["Category"] = x.CategoryName + " >> " + x.SubCategoryName;
+            dr["Resolved At"] = x.ResolvedAt;
+            dr["Closed At"] = x.ClosedAt;
+            dr["Details"] = x.Id;
+            dt.Rows.Add(dr);
         }
-        else if (_scope == "closed")
-        {
-            dt.Columns.Add(new DataColumn("Resolved At", typeof(string)));
-            dt.Columns.Add(new DataColumn("Closed At", typeof(string)));
-        }
-        dt.Columns.Add(new DataColumn("Details", typeof(string)));
+        gvTicketsClosed.DataSource = dt;
+        gvTicketsClosed.DataBind();
     }
 }
