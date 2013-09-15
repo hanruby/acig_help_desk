ff90fc830caf29d83d6af55b11a09b05e3843201
Ubaid Khan <ubaidkhan88@gmail.com>
2013-09-15 15:18:53 +0300

    added one more report

---
+++ Tickets/full_report2.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="full_report2.aspx.cs" Inherits="Tickets_full_report2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+<script type="text/javascript" src="../scripts/datepicker-range.js"></script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+  <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
+    <asp:UpdateProgress ID="updateProgress" runat="server">
+        <ProgressTemplate>
+            <div style="position: fixed; text-align: center; height: 100%; width: 100%; top: 0;
+                right: 0; left: 0; z-index: 9999999; background-color: #000000; opacity: 0.7;">
+                <asp:Image ID="imgUpdateProgress" runat="server" ImageUrl="~/images/ajax-loader.gif"
+                    AlternateText="Loading ..." ToolTip="Loading ..." Style="padding: 10px; position: fixed;
+                    top: 39%; left: 42%;" />
+            </div>
+        </ProgressTemplate>
+    </asp:UpdateProgress>
+    <asp:UpdatePanel ID="updatePanel" runat="server" UpdateMode="Conditional">
+        <ContentTemplate>
+            <div class="left">
+                <h3 style="margin: 0;">
+                    <a href="#">
+                        <asp:Label ID="lblTicketsCreated" runat="server" Text=""></asp:Label></a></h3>
+            </div>
+            <div class="right">
+                <asp:LinkButton ID="lnkBtnAllTC" runat="server" 
+                    onclick="lnkBtnAllTC_Click">All</asp:LinkButton>
+                &nbsp;
+                <asp:LinkButton ID="lnkBtnThisWeekTC" runat="server" 
+                    onclick="lnkBtnThisWeekTC_Click">This Week</asp:LinkButton>
+                &nbsp;
+                <asp:LinkButton ID="lnkBtnLastWeekTC" runat="server" 
+                    onclick="lnkBtnLastWeekTC_Click">Last Week</asp:LinkButton>
+                &nbsp;
+                <asp:LinkButton ID="lnkBtnThisMonthTC" runat="server" 
+                    onclick="lnkBtnThisMonthTC_Click">This Month</asp:LinkButton>
+                &nbsp;
+                <asp:LinkButton ID="lnkBtnLastMonthTC" runat="server" 
+                    onclick="lnkBtnLastMonthTC_Click">Last Month</asp:LinkButton>
+                &nbsp;
+                <asp:TextBox ID="txtStartDateTC" runat="server" CssClass="input-small start-date"></asp:TextBox>
+                 <asp:RequiredFieldValidator ID="rfvStartDateTC" runat="server" ControlToValidate="txtStartDateTC"
+                                ForeColor="#FF3300" SetFocusOnError="True" ValidationGroup="created">*</asp:RequiredFieldValidator>
+                <asp:TextBox ID="txtEndDateTC" runat="server" CssClass="input-small end-date"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvEndDateTC" runat="server" ControlToValidate="txtEndDateTC"
+                                ForeColor="#FF3300" SetFocusOnError="True" ValidationGroup="created">*</asp:RequiredFieldValidator>
+                <asp:Button ID="btnSearchTC" runat="server" Text="Search" 
+                    CssClass="btn btn-success" onclick="btnSearchTC_Click" ValidationGroup="created" />
+            </div>
+            <div class="clear">
+            </div>
+            <asp:Repeater ID="rptrFull" runat="server">
+                <HeaderTemplate>
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>
+                                Department
+                            </th>
+                            <th>
+                                Tickets
+                            </th>
+                        </tr>
+                </HeaderTemplate>
+                <ItemTemplate>
+                    <tr>
+                        <td>
+                            <%# Eval("email") %>
+                        </td>
+                        <asp:Repeater ID="rptrStates" runat="server" DataSource='<%# Eval("states") %>'>
+                            <HeaderTemplate>
+                                <td>
+                                    <table class="table table-bordered">
+                                        <tr>
+                                            <th>
+                                                State
+                                            </th>
+                                            <th>
+                                                Count
+                                            </th>
+                                        </tr>
+                            </HeaderTemplate>
+                            <ItemTemplate>
+                                <tr>
+                                    <td>
+                                        <%# Eval("state") %>
+                                    </td>
+                                    <td>
+                                        <%# Eval("count") %>
+                                    </td>
+                                </tr>
+                            </ItemTemplate>
+                            <FooterTemplate>
+                                </table> </td>
+                            </FooterTemplate>
+                        </asp:Repeater>
+                    </tr>
+                </ItemTemplate>
+                <FooterTemplate>
+                    </table> </hr>
+                </FooterTemplate>
+            </asp:Repeater>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
+
---
+++ Tickets/full_report2.aspx.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using Acig_Help_DeskModel;
+
+public partial class Tickets_full_report2 : MasterAppPage
+{
+    string role, role2;
+    Report_User rptUser;
+    Report_State rprtState;
+    List<Report_User> lstReportUsers;
+    string startDate, endDate;
+    DateTime startDateT, endDateT;
+    Hashtable hash;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            role = CurrentUser.Role();
+            role2 = CurrentUser.Role2();
+            if (role != "manager" && role2 != "admin")
+            {
+                ErrorRedirect(Route.GetRootPath("") + "not_authorized.aspx", "Not authorized to access that page !!");
+                return;
+            }
+            BindGridViewCustom("this_week");
+        }
+    }
+
+    protected void BindGridViewFull()
+    {
+        lblTicketsCreated.Text = "Tickets Created By Department !!";
+        txtStartDateTC.Text = string.Empty;
+        txtEndDateTC.Text = string.Empty;
+        _entity = GetEntity();
+        var data = from t in _entity.Tickets
+                   join u in _entity.tbl_Users
+                   on t.Created_By equals u.Id
+                   join d in _entity.Departments
+                   on u.Department_Id equals d.Id
+                   group t by new { u.Email, t.State } into Grp
+                   select new { Count = Grp.Count(), State = Grp.Key };
+
+        lstReportUsers = new List<Report_User>();
+        foreach (var x in data)
+        {
+            if (lstReportUsers.Where(y => y.email == x.State.Email).Count() > 0)
+            {
+                rptUser = lstReportUsers.Where(y => y.email == x.State.Email).First();
+            }
+            else
+            {
+                rptUser = new Report_User { email = x.State.Email };
+                lstReportUsers.Add(rptUser);
+            }
+            rprtState = rptUser.states.Where(y => y.state == x.State.State).First();
+            rprtState.count = x.Count;
+        }
+        rptrFull.DataSource = lstReportUsers;
+        rptrFull.DataBind();
+    }
+
+    protected void BindGridViewCustom(string type)
+    {
+        GetStartAndEndDate(type, "ticket_created");
+        txtStartDateTC.Text = startDate;
+        txtEndDateTC.Text = endDate;
+        lblTicketsCreated.Text = "Tickets Assigned to User From " + startDate + " To " + endDate + " !!";
+        _entity = GetEntity();
+        var data = from t in _entity.Tickets
+                   join u in _entity.tbl_Users
+                   on t.Created_By equals u.Id
+                   join d in _entity.Departments
+                   on u.Department_Id equals d.Id
+                   where t.Created_At >= startDateT && t.Created_At <= endDateT
+                   group t by new { u.Email, t.State } into Grp
+                   select new { Count = Grp.Count(), State = Grp.Key };
+
+        lstReportUsers = new List<Report_User>();
+        foreach (var x in data)
+        {
+            if (lstReportUsers.Where(y => y.email == x.State.Email).Count() > 0)
+            {
+                rptUser = lstReportUsers.Where(y => y.email == x.State.Email).First();
+            }
+            else
+            {
+                rptUser = new Report_User { email = x.State.Email };
+                lstReportUsers.Add(rptUser);
+            }
+            rprtState = rptUser.states.Where(y => y.state == x.State.State).First();
+            rprtState.count = x.Count;
+        }
+        rptrFull.DataSource = lstReportUsers;
+        rptrFull.DataBind();
+    }
+
+    protected void btnSearchTC_Click(object sender, EventArgs e)
+    {
+        BindGridViewCustom("custom");
+    }
+
+    protected void lnkBtnThisWeekTC_Click(object sender, EventArgs e)
+    {
+        BindGridViewCustom("this_week");
+    }
+
+    protected void lnkBtnLastWeekTC_Click(object sender, EventArgs e)
+    {
+        BindGridViewCustom("last_week");
+    }
+
+    protected void lnkBtnThisMonthTC_Click(object sender, EventArgs e)
+    {
+        BindGridViewCustom("this_month");
+    }
+
+    protected void lnkBtnLastMonthTC_Click(object sender, EventArgs e)
+    {
+        BindGridViewCustom("last_month");
+    }
+
+    protected void lnkBtnAllTC_Click(object sender, EventArgs e)
+    {
+        BindGridViewFull();
+    }
+
+    protected void GetStartAndEndDate(string type, string ticket_type)
+    {
+        DateTime baseDate = DateTime.Today;
+        var today = baseDate;
+        var yesterday = baseDate.AddDays(-1);
+        var thisWeekStart = baseDate.AddDays(-(int)baseDate.DayOfWeek);
+        var thisWeekEnd = thisWeekStart.AddDays(7).AddSeconds(-1);
+        var lastWeekStart = thisWeekStart.AddDays(-7);
+        var lastWeekEnd = thisWeekStart.AddSeconds(-1);
+        var thisMonthStart = baseDate.AddDays(1 - baseDate.Day);
+        var thisMonthEnd = thisMonthStart.AddMonths(1).AddSeconds(-1);
+        var lastMonthStart = thisMonthStart.AddMonths(-1);
+        var lastMonthEnd = thisMonthStart.AddSeconds(-1);
+        if (type == "this_week")
+        {
+            startDate = DateTimeHelper.ConvertToString(thisWeekStart.ToString());
+            endDate = DateTimeHelper.ConvertToString(thisWeekEnd.ToString());
+            startDateT = thisWeekStart;
+            endDateT = thisWeekEnd;
+        }
+        else if (type == "last_week")
+        {
+            startDate = DateTimeHelper.ConvertToString(lastWeekStart.ToString());
+            endDate = DateTimeHelper.ConvertToString(lastWeekEnd.ToString());
+            startDateT = lastWeekStart;
+            endDateT = lastWeekEnd;
+        }
+        else if (type == "this_month")
+        {
+            startDate = DateTimeHelper.ConvertToString(thisMonthStart.ToString());
+            endDate = DateTimeHelper.ConvertToString(thisMonthEnd.ToString());
+            startDateT = thisMonthStart;
+            endDateT = thisMonthEnd;
+        }
+        else if (type == "last_month")
+        {
+            startDate = DateTimeHelper.ConvertToString(lastMonthStart.ToString());
+            endDate = DateTimeHelper.ConvertToString(lastMonthEnd.ToString());
+            startDateT = lastMonthStart;
+            endDateT = lastMonthEnd;
+        }
+        else
+        {
+            startDate = txtStartDateTC.Text;
+            endDate = txtEndDateTC.Text;
+            hash = DateTimeHelper.GetStartAndEndDateByTwoValues(startDate, endDate);
+            startDateT = (DateTime)hash["StartDate"];
+            endDateT = (DateTime)hash["EndDate"];
+        }
+    }
+}
