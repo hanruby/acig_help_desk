266ce5db340e84c679e56e6e7b9a0b81c60d00d0
Ubaid Khan <ubaidkhan88@gmail.com>
2014-05-28 12:29:27 +0300

    fixed report by engineer

---
+++ App_Code/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class EnumHelper
+{
+    public enum TicketStates
+    {
+        Clarification,
+        Pending,
+        Clarified,
+        Closed,
+        Resolved
+    }
+}
--- Tickets/ereport.aspx.cs
+++ Tickets/ereport.aspx.cs
@@ -5,16 +5,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 public partial class Tickets_ereport : MasterAppPage
 {
-    string role, role2;
+    string role, role2, query, filterCondition;
     Report_User rptUser;
     Report_State rprtState;
     List<Report_User> lstReportUsers;
     string startDate, endDate;
     DateTime startDateT, endDateT;
     Hashtable hash;
+    SqlConnection conn = null;
+    SqlCommand cmd = null;
+    IDataReader dr = null;
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
@@ -36,30 +42,12 @@ public partial class Tickets_ereport : MasterAppPage
         lblTicketsCreated.Text = "Tickets Assigned To Engineer !!";
         txtStartDateTC.Text = string.Empty;
         txtEndDateTC.Text = string.Empty;
-        _entity = GetEntity();
-        var data = from t in _entity.Tickets
-                   join ut in _entity.User_Tickets
-                   on t.Id equals ut.Ticket_Id
-                   join u in _entity.tbl_Users
-                   on ut.User_Id equals u.Id
-                   group t by new { u.Email, t.State } into Grp
-                   select new { Count = Grp.Count(), State = Grp.Key };
 
         lstReportUsers = new List<Report_User>();
-        foreach (var x in data)
-        {
-            rptUser = lstReportUsers.Where(y => y.email == x.State.Email).FirstOrDefault();
-            if (rptUser == null)
-            {
-                rptUser = new Report_User { email = x.State.Email };
-                lstReportUsers.Add(rptUser);
-            }
-            rprtState = rptUser.states.Where(y => y.state == x.State.State).FirstOrDefault();
-            if (rprtState != null)
-            {
-                rprtState.count = x.Count;
-            }
-        }
+        AssignPendingQuery();
+        ExecuteQuery();
+        AssignResolvedQuery();
+        ExecuteQuery();
         rptrFull.DataSource = lstReportUsers;
         rptrFull.DataBind();
     }
@@ -70,31 +58,12 @@ public partial class Tickets_ereport : MasterAppPage
         txtStartDateTC.Text = startDate;
         txtEndDateTC.Text = endDate;
         lblTicketsCreated.Text = "Tickets Assigned to Engineer From " + startDate + " To " + endDate + " !!";
-        _entity = GetEntity();
-        var data = from t in _entity.Tickets
-                   join ut in _entity.User_Tickets
-                   on t.Id equals ut.Ticket_Id
-                   join u in _entity.tbl_Users
-                   on ut.User_Id equals u.Id
-                   where t.Created_At >= startDateT && t.Created_At <= endDateT
-                   group t by new { u.Email, t.State } into Grp
-                   select new { Count = Grp.Count(), State = Grp.Key };
 
         lstReportUsers = new List<Report_User>();
-        foreach (var x in data)
-        {
-            rptUser = lstReportUsers.Where(y => y.email == x.State.Email).FirstOrDefault();
-            if (rptUser == null)
-            {
-                rptUser = new Report_User { email = x.State.Email };
-                lstReportUsers.Add(rptUser);
-            }
-            rprtState = rptUser.states.Where(y => y.state == x.State.State).FirstOrDefault();
-            if (rprtState != null)
-            {
-                rprtState.count = x.Count;
-            }
-        }
+        AssignPendingQuery(true);
+        ExecuteQuery(true);
+        AssignResolvedQuery(true);
+        ExecuteQuery(true);
         rptrFull.DataSource = lstReportUsers;
         rptrFull.DataBind();
     }
@@ -177,6 +146,73 @@ public partial class Tickets_ereport : MasterAppPage
             hash = DateTimeHelper.GetStartAndEndDateByTwoValues(startDate, endDate);
             startDateT = (DateTime)hash["StartDate"];
             endDateT = (DateTime)hash["EndDate"];
+        }
+    }
+
+    void AssignPendingQuery(bool filter = false)
+    {
+        filterCondition = filter ? "WHERE evc.Created_At >= @Start_Date AND evc.Created_At <= @End_Date" : string.Empty;
+        query = "SELECT t.State, r.User_Name, evc.Created_At FROM TICKETS t INNER JOIN " +
+            "(SELECT e.Created_By, e.Ticket_Id, e.State, e.Created_At FROM events e INNER JOIN " +
+            "(SELECT MAX(id) as CID FROM events GROUP BY ticket_id  ) ec " +
+            "ON e.Id = ec.CID WHERE e.State = 'Open') evc ON t.Id = evc.Ticket_Id " +
+            "INNER JOIN User_Tickets ut ON t.Id = ut.Ticket_Id INNER JOIN tbl_Users r " +
+            "ON ut.User_Id = r.Id {0} ORDER BY t.Id, r.User_Name";
+        query = string.Format(query, filterCondition);
+    }
+
+    void AssignResolvedQuery(bool filter = false)
+    {
+        filterCondition = filter ? "WHERE evc.Created_At >= @Start_Date AND evc.Created_At <= @End_Date" : string.Empty;
+        query = "SELECT t.State, r.User_Name, evc.Created_At FROM TICKETS t INNER JOIN " +
+            "(SELECT e.Created_By, e.Ticket_Id, e.State, e.Created_At FROM events e INNER JOIN " +
+            "(SELECT MAX(id) as CID FROM events WHERE State = 'Resolved' GROUP BY ticket_id  ) ec " +
+            "ON e.Id = ec.CID) evc ON t.Id = evc.Ticket_Id INNER JOIN tbl_Users r ON evc.Created_By = r.Id " +
+            "{0} ORDER BY t.Id, r.User_Name";
+        query = string.Format(query, filterCondition);
+    }
+
+    void ExecuteQuery(bool filter = false)
+    {
+        conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Acig_Help_DeskConnectionString"].ToString());
+        cmd = new SqlCommand();
+        try
+        {
+            conn.Open();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = conn;
+            cmd.CommandText = query;
+            if (filter)
+            {
+                cmd.Parameters.AddWithValue("@Start_Date", startDateT);
+                cmd.Parameters.AddWithValue("@End_Date", startDateT);
+            }
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                rptUser = lstReportUsers.Where(y => y.email == dr["User_Name"].ToString()).FirstOrDefault();
+                if (rptUser == null)
+                {
+                    rptUser = new Report_User { email = dr["User_Name"].ToString() };
+                    lstReportUsers.Add(rptUser);
+                }
+                rprtState = rptUser.states.Where(y => y.state == dr["State"].ToString()).FirstOrDefault();
+                if (rprtState == null)
+                {
+                    rprtState = new Report_State { state = dr["State"].ToString(), count = 0 };
+                    rptUser.states.Add(rprtState);
+                }
+                rprtState.count += 1;
+            }
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            cmd.Parameters.Clear();
+            conn.Close();
         }
     }
 }
